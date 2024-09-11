491da089d26b9266126a5308d2e8667c370b8c78
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-09-11 15:04:09 +0200

    Adding handler

---
+++ handler/tracelog.js
@@ -0,0 +1,108 @@
+const url = require('url');
+const cache = require('../lib');
+const logger = require('../lib/util').getLogger();
+
+async function handle(req,res,options) {
+    const parsedUrl = url.parse(req.url,true);
+    const queryObject = parsedUrl.query;
+    const artifact = queryObject.artifact;
+
+    if (parsedUrl.pathname.includes("/urn:uuid")) {
+        return handleEvent(req,res,options);
+    }
+
+    res.setHeader('Content-Type','application/ls+json');
+
+    if (! artifact) {
+        res.writeHead(404);
+        res.end(JSON.stringify({ error : `need artifact parameter`}));
+        return;
+    }
+   
+    const events = await resolveEventLog(artifact);
+
+    if (! events) {
+        res.writeHead(404);
+        res.end(JSON.stringify({ error : `no events for ${artifact}`}));
+        return;
+    }
+
+    const traceLog = {
+        "@context" : "https://labs.eventnotifications.net/contexts/eventlog.jsonld",
+        "id": `${process.env.EVENTLOG_BASEURL}${req.url}`, 
+        "type": "TraceLog",
+        "artifact": artifact,
+        "member": []
+    };
+
+    for (let i = 0 ; i < events.length ; i++) {
+        traceLog.member.push({
+            id: `${process.env.EVENTLOG_BASEURL}${parsedUrl.pathname}/${events[i].id}`
+        });
+    }
+
+    res.writeHead(200);
+    res.end(JSON.stringify(traceLog));
+}
+
+async function handleEvent(req,res,options) {
+    const parsedUrl = url.parse(req.url,true);
+    const id = parsedUrl.pathname.replace(/\/trace\//,'');
+
+    const event = await resolveEvent(id);
+    
+    res.setHeader('Content-Type','application/ls+json');
+
+    if (! event) {
+        res.writeHead(404);
+        res.end(JSON.stringify({ error : `need such event ${id}`}));
+        return;
+    }
+
+    res.writeHead(200);
+    res.end(JSON.stringify(event));
+}
+
+async function resolveEvent(id) {
+    logger.debug(`resolveEvent(${id})`);
+    try {
+        const event = await cache.getCache(id);
+        return event;
+    }
+    catch (e) {
+        logger.error(e);
+        return null; 
+    }
+}
+
+async function resolveEventLog(url) {
+    logger.debug(`resolveEventLog(${url})`);
+    try {
+        const events = await cache.listCache(`object.id=${url}`);
+
+        if (events.length == 0) {
+            return null;
+        }
+
+        const latest = events.at(-1);
+
+        const related = await cache.listCache('',`original=${latest}`);
+
+        const trace = [ latest ].concat(related);
+
+        const resolved = [];
+
+        for (let i = 0 ; i < trace.length ; i++) {
+            const event = await cache.getCache(trace[i]); 
+            resolved.push(event);      
+        }
+
+        return resolved;
+    }
+    catch (e) {
+        logger.error(e);
+        return null;
+    }
+}
+
+module.exports = { handle };
